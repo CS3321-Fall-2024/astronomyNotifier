affe38c9f63ca4cbda9e53d49c67e885ff55540f
prashant9711 <prashantpant@isu.edu>
2024-11-17 11:01:29 -0700

    Fixing the functions in main

--- src/main.py
+++ src/main.py
@@ -10,19 +10,21 @@ async def hello():
     return "hello world!"
    
 
-@app.route('/test5')
+@app.route('/get_moon_phase')
 async def test6():
     s = await get_moon_phase(datetime.utcnow())
     
     return "The moon phase forecast for the next 7 days is: " +str(s)
     
 
-@app.route('/test6')
+# takes long, lat as arg to return daily weather for the next 7 days
+@app.route('/fetch_daily_weather')
 async def test7():
     s = await fetch_daily_weather(44.0682,-114.7420)
     return str(s)
 
-@app.route('/test7')
+# takes long, lat as arg to return hourly weather for the next 7 days
+@app.route('/fetch_hourly_weather')
 async def test8():
     s  = await fetch_hourly_weather(44.0682,-114.7420)
     return str(s)
