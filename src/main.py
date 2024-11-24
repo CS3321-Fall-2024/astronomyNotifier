1f33dd9d8b52756ec0f74d1ae96e57274fb65d0a
Andrija-Sevaljevic <andrijasevaljevic@isu.edu>
2024-11-23 21:36:11 -0700

    bug fixed

--- src/main.py
+++ src/main.py
@@ -1,85 +1,99 @@
 from quart import Quart, jsonify
 from apis.weatherQueries import *
+from datetime import datetime
 
 app = Quart(__name__)
 
+def write_to_file(data):
+    with open('example.txt', 'a') as f:
+        f.write(f"{data}\n")
 
 @app.route("/get_asteroids")
-async def hello():
-
+async def test1():
     api_key = "cyA6A9N1vmADe8FxrOi7gg5jqs5EhGJCKC9Bclt1"
-    asteroids = await get_asteroids(api_key)
+    asteroidsList = await get_asteroids(api_key)
+    
+    asteroids = [asteroid for asteroid in asteroidsList if asteroid["close_approach_data"]]
 
     sorted_by_magnitude = sorted(asteroids, key=lambda a: a["absolute_magnitude_h"])
-
     sorted_by_distance = sorted(
         asteroids,
         key=lambda a: a["close_approach_data"][0]["miss_distance"]["kilometers"],
     )
 
-    magnitude_response = "Asteroids sorted by magnitude:<br>"
+    magnitude_response = "Asteroids sorted by magnitude:\n"
     for asteroid in sorted_by_magnitude:
         magnitude = asteroid["absolute_magnitude_h"]
         name = asteroid["name"]
-        magnitude_response += f"{name}: Magnitude {magnitude}<br>"
+        magnitude_response += f"{name}: Magnitude {magnitude}\n"
+        
 
-    distance_response = "Asteroids sorted by distance:<br>"
+    distance_response = "Asteroids sorted by distance:\n"
     for asteroid in sorted_by_distance:
         distance = asteroid["close_approach_data"][0]["miss_distance"]["kilometers"]
         name = asteroid["name"]
-        distance_response += f"{name}: Distance {distance} km<br>"
+        distance_response += f"{name}: Distance {distance} km\n"
 
-    return magnitude_response + "<br>" + distance_response
-
+    result = magnitude_response + "\n~\n\n" + distance_response
+    result += "\n^\n"
+    write_to_file(result)
+    return result
 
 @app.route("/get_next_eclipse")
-async def test1():
+async def test2():
     lat, lon = await get_current_location()
     next_solar = await get_next_eclipse(lat, lon)
     
     if next_solar:
-        result = f"Next Solar Eclipse:<br>"
-        result += f"Type: {next_solar.kind.name}<br>"
-        result += f"Visibility: {next_solar.obscuration}<br>"
-        result += f"Date: {datetime.fromisoformat(str(next_solar.peak.time)).strftime('%Y-%m-%d %H:%M:%S')}<br>"
+        result = f"Next Solar Eclipse:\n"
+        result += f"Type: {next_solar.kind.name}\n"
+        result += f"Visibility: {next_solar.obscuration}\n"
+        result += f"Date: {datetime.fromisoformat(str(next_solar.peak.time)).strftime('%Y-%m-%d %H:%M:%S')}\n"
     else:
-        result = "No upcoming solar eclipses.<br>"
+        result = "No upcoming solar eclipses.\n"
 
+    result += "\n^\n"
+    write_to_file(result)
     return result
 
-
 @app.route("/get_next_iss_pass")
-async def test2():
+async def test3():
     lat, lon = await get_current_location()
     if lat is None or lon is None:
         print("Unable to fetch location")
         return
 
-    # Get the next 5 ISS passes within the next 7 days
     passes = get_next_iss_pass(lat, lon, p=5, d=7)
     if passes:
-        result = "Upcoming ISS Passes:<br>\n"
+        result = "Upcoming ISS Passes:\n"
         for idx, pass_time in enumerate(passes, 1):
-            result += f"Pass {idx}: {pass_time}<br>\n"
-        return result
+            result += f"Pass {idx}: {pass_time}\n"
     else:
-        return f"No passes found within the provided days."
-    
+        result = "No passes found within the provided days."
+
+    result += "\n^\n"
+    write_to_file(result)
+    return result
+
 @app.route("/get_distance_to_iss")
-async def test3():
+async def test4():
     lat, lon = await get_current_location()
     distance, lat_iss, lon_iss = await get_distance_to_iss(lat, lon)
-    result = f"The ISS is approximately {distance:.2f} kilometers away from your location.<br>"
-    result += f"The ISS is currently at latitude {lat_iss:.2f} and longitude {lon_iss:.2f}.<br>"
+    result = f"The ISS is approximately {distance:.2f} kilometers away from your location.\n"
+    result += f"The ISS is currently at latitude {lat_iss:.2f} and longitude {lon_iss:.2f}.\n"
+    
+    result += "\n^\n"
+    write_to_file(result)
     return result
 
 @app.route("/get_nasa_picture_of_the_day")
-async def get_nasa_picture_of_the_day():
+async def test5():
     api_key = "cyA6A9N1vmADe8FxrOi7gg5jqs5EhGJCKC9Bclt1"
     result = await get_nasa_picture_of_the_day(api_key)
-    return result
     
-
+    result += "\n^\n"
+    write_to_file(result)
+    return result
 
 if __name__ == "__main__":
     app.run()
--- tests/test_app.py
+++ tests/test_app.py
@@ -1,29 +1,8 @@
 import asyncio
 import pytest
-from main import hello, test1, test2
+from main import test1, test2, test3, test4, test5
 
 pytest_plugins = ("pytest_asyncio",)
-
-#@pytest.mark.parametrize(
-#     "expected",
-#     [
-#         pytest.param("Pocatello", id="Getting the city name"), 
-#         ],
-# )
-# 
-# 
-# def test_get_current_city(expected):
-#     assert app.get_current_city() == expected
-
-@pytest.mark.asyncio
-@pytest.mark.parametrize(
-        "expected",
-        [
-            pytest.param("hello world!", id="init test"),
-            ],
-)
-async def test_hello(expected):
-    assert await hello() == expected
 
 @pytest.mark.asyncio
 @pytest.mark.parametrize(
@@ -44,3 +23,33 @@ async def test1_Call(expected):
 )
 async def test2_Call(expected):
     assert await test2() == expected 
+    
+@pytest.mark.asyncio
+@pytest.mark.parametrize(
+        "expected",
+        [
+            pytest.param("This is a message from test2.", id="Test message for test1"),
+        ],
+)
+async def test3_Call(expected):
+    assert await test3() == expected 
+    
+@pytest.mark.asyncio
+@pytest.mark.parametrize(
+        "expected",
+        [
+            pytest.param("This is a message from test2.", id="Test message for test1"),
+        ],
+)
+async def test4_Call(expected):
+    assert await test4() == expected 
+    
+@pytest.mark.asyncio
+@pytest.mark.parametrize(
+        "expected",
+        [
+            pytest.param("This is a message from test2.", id="Test message for test1"),
+        ],
+)
+async def test5_Call(expected):
+    assert await test5() == expected 
